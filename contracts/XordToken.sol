0990c449ba7880ea042efe2772a1144b64d72731
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2021-03-05 12:47:07 +0500

    added Approval event and transferFrom function

--- contracts/XordToken.sol
+++ contracts/XordToken.sol
@@ -17,8 +17,14 @@ contract XordToken{
         address indexed _to,
         uint256 _value
     );
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
     mapping(address => uint256) public balanceOf;
-    
+    // allowance mapping
+    mapping(address => mapping(address => uint256)) public allowance;
     constructor(uint256 _initialSupply) public{
         balanceOf[msg.sender]=_initialSupply;
         totalSupply = _initialSupply;       
@@ -29,12 +35,37 @@ contract XordToken{
     function transfer(address _to, uint256 _value)public returns(bool success){
     // Trigger an Exception if account doesn't have enough
     require(balanceOf[msg.sender]>=_value);
-    // Return a boolean
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
     //Transfer Event    
     emit Transfer(msg.sender, _to, _value);
+    // Return a boolean
     return true;
+    }
+
+    // Approve
+    function approve(address _spender, uint256 _value)public returns(bool success){
+        //allowance
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    // Transfer From
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        // Require _from has enough tokens
+        require(_value <= balanceOf[_from]);
+        // Require the allowance is big enough
+        require(_value <= allowance[_from][msg.sender]);
+        // Change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] +=_value;
+        // Update the allowance
+        allowance[_from][msg.sender]-=_value;
+        // Transfer event
+        emit Transfer(_from, _to, _value);
+        // Returns a boolean
+        return true;
     }
 
 }
--- test/XordToken.js
+++ test/XordToken.js
@@ -12,11 +12,11 @@ contract("XordToken", function (accounts) {
         return tokenInstance.name();
       })
       .then(function (name) {
-        assert.equal(name, "Xord", "has the correct name");
+        assert.equal(name, "Water", "has the correct name");
         return tokenInstance.symbol();
       })
       .then(function (symbol) {
-        assert.equal(symbol, "XORD", "has the correct symbol");
+        assert.equal(symbol, "WATER", "has the correct symbol");
       });
   });
 
@@ -42,18 +42,84 @@ contract("XordToken", function (accounts) {
         );
       });
   });
-
+  /*
   it("Transfers ownership", function () {
+    return XordToken.deployed().then(function (instance) {
+      tokenInstance = instance;
+      return tokenInstance.trasfer.call(accounts[1], 999999999999999999);
+    });
+  });
+*/
+
+  it("Approves tokens for delegated transfer", function () {
     return XordToken.deployed()
       .then(function (instance) {
         tokenInstance = instance;
-        return tokenInstance.trasfer.call(accounts[1], 999999999999999999);
+        return tokenInstance.approve.call(accounts[1], 100);
+      })
+      .then(function (success) {
+        assert.equal(success, true, "it returns true");
+        return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+      })
+      .then(function (receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Approval",
+          'should be the "Approval" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._owner,
+          accounts[0],
+          "logs the account the token are authorized by"
+        );
+        assert.equal(
+          receipt.logs[0].args._spender,
+          accounts[1],
+          "logs the account the token are authorized to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          100,
+          "logs the transfer amount"
+        );
+        return tokenInstance.allowance(accounts[0], accounts[1]);
+      })
+      .then(function (allowance) {
+        assert.equal(
+          allowance.toNumber(),
+          100,
+          "stores the allowance for delegated transfer"
+        );
+      });
+  });
+
+  it("handles delegated token transfers", function () {
+    return XordToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+        //Transfer some tokens to fromAccount
+        return tokenInstance.tranfer(fromAccount, 100, { from: accounts[0] });
+      })
+      .then(function (receipt) {
+        // Approve spendingAcount to spend 10 tokens fromAccount
+        return tokenInstance.approve(spendingAccount, 10, {
+          from: fromAccount,
+        });
+      })
+      .then(function (receipt) {
+        return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {
+          from: spendingAccount,
+        });
       })
       .then(assert.fail)
       .catch(function (error) {
         assert(
           error.message.indexOf("revert") >= 0,
-          "error message must contain revert"
+          "cannot transfer value larger than balance"
         );
       });
   });
